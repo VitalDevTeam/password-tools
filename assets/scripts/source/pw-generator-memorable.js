c5837d0b1a7915da0f4844a550eb022b5255cd15
Adam Walter <adam@vtldesign.com>
2017-12-20 16:17:19 -0500

    Adds hyphens to memorable passwords to make them stronger and more compatible

--- assets/scripts/source/pw-generator-memorable.js
+++ assets/scripts/source/pw-generator-memorable.js
@@ -3,24 +3,30 @@
  * http://www.dinopass.com/api
  */
 var passwordGeneratorMemorable = (function($) {
-    var memPassordSubmit, memPasswordOutput;
+    var memPasswordSubmit, memPasswordOutput;
 
+	/**
+	 * Generates a random memorable password using the WornNik API
+	 */
     function getMemorablePassword() {
         var request = new XMLHttpRequest();
 
         request.open('GET', 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=4&maxLength=4&limit=2&api_key=e6243ae1ce542700b82e51599530d5e76669c3e585635f6b7', true);
 
         request.onload = function() {
-            var pw = '';
+            var pw = '',
+				specialChars = ['!', '@', '#', '$', '%', '&', '?'];
             if (request.status >= 200 && request.status < 400) {
                 var data = JSON.parse(request.responseText);
                 for (var i in data) { // jshint ignore:line
-                    pw += data[i].word;
+                    pw += data[i].word + '-';
                 }
                 pw += Math.floor(Math.random() * 90 + 10);
                 memPasswordOutput.value = pw;
+				memPasswordSubmit.removeAttribute('disabled');
             } else {
                 memPasswordOutput.value = "Damn thing is busted.";
+				memPasswordSubmit.removeAttribute('disabled');
             }
         };
 
@@ -32,7 +38,7 @@ var passwordGeneratorMemorable = (function($) {
     }
 
     function onDocumentReady() {
-        memPassordSubmit = document.getElementById('pw-generator-mem-submit');
+        memPasswordSubmit = document.getElementById('pw-generator-mem-submit');
         memPasswordOutput = document.getElementById('pw-generator-mem-output');
 
         getMemorablePassword();
@@ -41,7 +47,10 @@ var passwordGeneratorMemorable = (function($) {
             this.select();
         });
 
-        memPassordSubmit.addEventListener('click', getMemorablePassword);
+        memPasswordSubmit.addEventListener('click', function() {
+			memPasswordSubmit.setAttribute('disabled', 'true');
+			getMemorablePassword();
+		});
     }
 
     $(onDocumentReady);
